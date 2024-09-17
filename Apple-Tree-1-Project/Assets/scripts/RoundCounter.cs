b82c11e6eba859756ad3a92617188c5a93c247f0
HarrisonFeco <84511537+HarrisonFeco@users.noreply.github.com>
2024-09-16 22:46:27 -0500

    Round counter
    Started to create a counter at the top

---
+++ Apple-Tree-1-Project/Assets/scripts/RoundCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundCounter : MonoBehaviour
+{
+     static private Text _UI_TEXT;
+    static private int _SCORE = 1000;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
